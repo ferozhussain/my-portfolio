b322d60f828139bd585cf4b2ecae34af0fc1f335
sloppy_dev <78643806+ferozhussain@users.noreply.github.com>
2025-02-15 23:34:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your repository name
+  base: '/my-portfolio/', // Replace 'portfolio' with your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
